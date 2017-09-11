eb821952754b9de16b4b92419e3c7522477c2516
ezio84 <brabus84@gmail.com>
2017-09-11 06:46:31 +0530

    Battery light customization [2/2]
    aex edits: Add back disable battery light toggle
    for obvious reason, also set other options dependencies
    on it
    
    Change-Id: I792c6428a774fb92b981e4e248d024c45acd6f4e
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/bootleggers/dumpster/fragments/NotificationSettings.java
+++ src/com/bootleggers/dumpster/fragments/NotificationSettings.java
@@ -2,18 +2,69 @@ package com.bootleggers.dumpster.fragments;
 
 import com.android.internal.logging.nano.MetricsProto;
 
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.UserInfo;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.os.UserManager;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
 import com.android.settings.R;
+import android.support.annotation.NonNull;
 
+import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.SettingsPreferenceFragment;
+import com.bootleggers.dumpster.preferences.SystemSettingSwitchPreference;
 
-public class NotificationSettings extends SettingsPreferenceFragment {
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationSettings extends SettingsPreferenceFragment
+                    implements Preference.OnPreferenceChangeListener {
+
+    private PreferenceCategory mLedsCategory;
+    private Preference mChargingLeds;
+    private SystemSettingSwitchPreference mLowBatteryBlinking;
 
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
 
         addPreferencesFromResource(R.xml.bootleg_dumpster_notifications);
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        mLedsCategory = (PreferenceCategory) findPreference("light_category");
+        mChargingLeds = (Preference) findPreference("battery_charging_light");
+        if (mChargingLeds != null
+                && !getResources().getBoolean(
+                        com.android.internal.R.bool.config_intrusiveBatteryLed)) {
+            mLedsCategory.removePreference(mChargingLeds);
+        }
+          if (mChargingLeds == null) {
+            prefSet.removePreference(mLedsCategory);
+        }
+        mLowBatteryBlinking = (SystemSettingSwitchPreference)prefSet.findPreference("battery_light_low_blinking");
+        if (getResources().getBoolean(
+                        com.android.internal.R.bool.config_ledCanPulse)) {
+            mLowBatteryBlinking.setChecked(Settings.System.getIntForUser(getContentResolver(),
+                            Settings.System.BATTERY_LIGHT_LOW_BLINKING, 0, UserHandle.USER_CURRENT) == 1);
+            mLowBatteryBlinking.setOnPreferenceChangeListener(this);
+        } else {
+            mLedsCategory.removePreference(mLowBatteryBlinking);
+        }
+
+    }
+    
+    public boolean onPreferenceChange(Preference preference, Object value) {
+         return true;
     }
 
     @Override
