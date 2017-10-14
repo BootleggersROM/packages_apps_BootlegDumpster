5ddddf6f367051e32a58a5025ead270f3431bb96
beanstown106 <nbenis106@gmail.com>
2017-10-14 23:17:23 +0530

    Dumpster: Incall Vibration Options [3/3]
      * allow setting vibration when call is connected
      * allow setting vibration when call is disconnected
      * allow setting vibration for call waiting
      * this works with google and aosp dialer :)
    
    Change-Id: Id1a431124174571f8d0091825af2418971cfe35d
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/bootleggers/dumpster/fragments/MiscSettings.java
+++ src/com/bootleggers/dumpster/fragments/MiscSettings.java
@@ -1,7 +1,5 @@
 package com.bootleggers.dumpster.fragments;
 
-import com.android.internal.logging.nano.MetricsProto;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
@@ -15,19 +13,24 @@ import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
 import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
-import com.android.settings.R;
 
 import java.util.Arrays;
 import java.util.HashSet;
 
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
 
 public class MiscSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private static final String INCALL_VIB_OPTIONS = "incall_vib_options";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -35,6 +38,12 @@ public class MiscSettings extends SettingsPreferenceFragment implements
 
         addPreferencesFromResource(R.xml.bootleg_dumpster_misc);
 
+        final PreferenceScreen prefSet = getPreferenceScreen();
+
+        PreferenceCategory incallVibCategory = (PreferenceCategory) findPreference(INCALL_VIB_OPTIONS);
+        if (!Utils.isVoiceCapable(getActivity())) {
+            prefSet.removePreference(incallVibCategory);
+        }
     }
 
     @Override
