HassanSardar/packages_apps_WolvesDen

WIP:Settings : Hardware key rebinding (2/2)
 * need to add back key
 * stuff

Change-Id: I1a0546f358e787a9c6ab12024e34255475259424

src/com/gzr/wolvesden/fragments/ButtonSettings.java
@@ -1,0 +1,231 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gzr.wolvesden.fragments;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.Handler;
+import android.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.IWindowManager;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.WindowManagerGlobal;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.gzosp.GzospUtils;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ButtonSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_HOME_LONG_PRESS = "hardware_keys_home_long_press";
+    private static final String KEY_HOME_DOUBLE_TAP = "hardware_keys_home_double_tap";
+    private static final String KEY_MENU_PRESS = "hardware_keys_menu_press";
+    private static final String KEY_MENU_DOUBLE_TAP = "hardware_keys_menu_double_tap";
+    private static final String KEY_MENU_LONG_PRESS = "hardware_keys_menu_long_press";
+    private static final String KEY_BACK_PRESS = "hardware_keys_back_press";
+    private static final String KEY_BACK_DOUBLE_TAP = "hardware_keys_back_double_tap";
+
+    private static final String CATEGORY_POWER = "power_key";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+    private static final String CATEGORY_CAMERA = "camera_key";
+    private static final String CATEGORY_VOLUME = "volume_keys";
+    private static final String CATEGORY_BACKLIGHT = "key_backlight";
+    private static final String CATEGORY_NAVBAR = "navigation_bar";
+
+    // Available custom actions to perform on a key press.
+    // Must match values for KEY_HOME_LONG_PRESS_ACTION in:
+    // frameworks/base/core/java/android/provider/Settings.java
+    private static final int ACTION_NOTHING = 0;
+    private static final int ACTION_MENU = 1;
+    private static final int ACTION_APP_SWITCH = 2;
+    private static final int ACTION_SEARCH = 3;
+    private static final int ACTION_VOICE_SEARCH = 4;
+    private static final int ACTION_IN_APP_SEARCH = 5;
+    private static final int ACTION_LAUNCH_CAMERA = 6;
+    private static final int ACTION_LAST_APP = 7;
+    private static final int ACTION_SLEEP = 8;
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+
+    private ListPreference mHomeLongPressAction;
+    private ListPreference mHomeDoubleTapAction;
+    private ListPreference mMenuPressAction;
+    private ListPreference mMenuLongPressAction;
+    private ListPreference mMenuDoubleTapAction;
+    private ListPreference mBackPressAction;
+    private ListPreference mBackDoubleTapAction;
+
+    private PreferenceCategory mNavigationPreferencesCat;
+
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.button_settings);
+
+        final Resources res = getResources();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        final boolean hasPowerKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_POWER);
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+
+        boolean hasAnyBindableKey = false;
+        final PreferenceCategory homeCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
+        final PreferenceCategory backCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_BACK);
+
+        mHandler = new Handler();
+
+        if (hasHomeKey) {
+            int defaultLongPressAction = res.getInteger(
+                    com.android.internal.R.integer.config_longPressOnHomeBehavior);
+            if (defaultLongPressAction < ACTION_NOTHING ||
+                    defaultLongPressAction > ACTION_IN_APP_SEARCH) {
+                defaultLongPressAction = ACTION_NOTHING;
+            }
+
+            int defaultDoubleTapAction = res.getInteger(
+                    com.android.internal.R.integer.config_doubleTapOnHomeBehavior);
+            if (defaultDoubleTapAction < ACTION_NOTHING ||
+                    defaultDoubleTapAction > ACTION_IN_APP_SEARCH) {
+                defaultDoubleTapAction = ACTION_NOTHING;
+            }
+
+            int longPressAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_HOME_LONG_PRESS_ACTION,
+                    defaultLongPressAction);
+            mHomeLongPressAction = initActionList(KEY_HOME_LONG_PRESS, longPressAction);
+
+            int doubleTapAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_HOME_DOUBLE_TAP_ACTION,
+                    defaultDoubleTapAction);
+            mHomeDoubleTapAction = initActionList(KEY_HOME_DOUBLE_TAP, doubleTapAction);
+
+            hasAnyBindableKey = true;
+        } else {
+            prefScreen.removePreference(homeCategory);
+        }
+
+        if (hasMenuKey) {
+            int pressAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_MENU_ACTION, ACTION_MENU);
+            mMenuPressAction = initActionList(KEY_MENU_PRESS, pressAction);
+
+            int longPressAction = Settings.System.getInt(resolver,
+                        Settings.System.KEY_MENU_LONG_PRESS_ACTION,
+                        hasAssistKey ? ACTION_NOTHING : ACTION_SEARCH);
+            mMenuLongPressAction = initActionList(KEY_MENU_LONG_PRESS, longPressAction);
+
+            hasAnyBindableKey = true;
+        } else {
+            prefScreen.removePreference(menuCategory);
+        }
+
+        if (!hasBackKey) {
+            prefScreen.removePreference(backCategory);
+        }
+    }
+
+    private ListPreference initActionList(String key, int value) {
+        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
+        list.setValue(Integer.toString(value));
+        list.setSummary(list.getEntry());
+        list.setOnPreferenceChangeListener(this);
+        return list;
+    }
+
+    private void handleActionListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
+
+        pref.setSummary(pref.getEntries()[index]);
+        Settings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.VALIDUS;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHomeLongPressAction) {
+            handleActionListChange(mHomeLongPressAction, newValue,
+                    Settings.System.KEY_HOME_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mHomeDoubleTapAction) {
+            handleActionListChange(mHomeDoubleTapAction, newValue,
+                    Settings.System.KEY_HOME_DOUBLE_TAP_ACTION);
+            return true;
+        } else if (preference == mMenuPressAction) {
+            handleActionListChange(mMenuPressAction, newValue,
+                    Settings.System.KEY_MENU_ACTION);
+            return true;
+        } else if (preference == mMenuLongPressAction) {
+            handleActionListChange(mMenuLongPressAction, newValue,
+                    Settings.System.KEY_MENU_LONG_PRESS_ACTION);
+            return true;
+        }
+        return false;
+    }
+
+}

BootleggersROM/packages_apps_BootlegDumpster

src/com/bootleggers/dumpster/ButtonSettings.java
@@ -1,0 +1,231 @@
+/*
+ * Copyright (C) 2013 The CyanogenMod project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.bootleggers.dumpster;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.os.Handler;
+import android.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.IWindowManager;
+import android.view.KeyCharacterMap;
+import android.view.KeyEvent;
+import android.view.WindowManagerGlobal;
+
+import com.android.internal.logging.nano.MetricsProto;
+import com.android.internal.util.gzosp.GzospUtils;
+
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class ButtonSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_HOME_LONG_PRESS = "hardware_keys_home_long_press";
+    private static final String KEY_HOME_DOUBLE_TAP = "hardware_keys_home_double_tap";
+    private static final String KEY_MENU_PRESS = "hardware_keys_menu_press";
+    private static final String KEY_MENU_DOUBLE_TAP = "hardware_keys_menu_double_tap";
+    private static final String KEY_MENU_LONG_PRESS = "hardware_keys_menu_long_press";
+    private static final String KEY_BACK_PRESS = "hardware_keys_back_press";
+    private static final String KEY_BACK_DOUBLE_TAP = "hardware_keys_back_double_tap";
+
+    private static final String CATEGORY_POWER = "power_key";
+    private static final String CATEGORY_HOME = "home_key";
+    private static final String CATEGORY_MENU = "menu_key";
+    private static final String CATEGORY_BACK = "back_key";
+    private static final String CATEGORY_ASSIST = "assist_key";
+    private static final String CATEGORY_APPSWITCH = "app_switch_key";
+    private static final String CATEGORY_CAMERA = "camera_key";
+    private static final String CATEGORY_VOLUME = "volume_keys";
+    private static final String CATEGORY_BACKLIGHT = "key_backlight";
+    private static final String CATEGORY_NAVBAR = "navigation_bar";
+
+    // Available custom actions to perform on a key press.
+    // Must match values for KEY_HOME_LONG_PRESS_ACTION in:
+    // frameworks/base/core/java/android/provider/Settings.java
+    private static final int ACTION_NOTHING = 0;
+    private static final int ACTION_MENU = 1;
+    private static final int ACTION_APP_SWITCH = 2;
+    private static final int ACTION_SEARCH = 3;
+    private static final int ACTION_VOICE_SEARCH = 4;
+    private static final int ACTION_IN_APP_SEARCH = 5;
+    private static final int ACTION_LAUNCH_CAMERA = 6;
+    private static final int ACTION_LAST_APP = 7;
+    private static final int ACTION_SLEEP = 8;
+
+    // Masks for checking presence of hardware keys.
+    // Must match values in frameworks/base/core/res/res/values/config.xml
+    public static final int KEY_MASK_HOME = 0x01;
+    public static final int KEY_MASK_BACK = 0x02;
+    public static final int KEY_MASK_MENU = 0x04;
+    public static final int KEY_MASK_ASSIST = 0x08;
+    public static final int KEY_MASK_APP_SWITCH = 0x10;
+    public static final int KEY_MASK_CAMERA = 0x20;
+
+    private ListPreference mHomeLongPressAction;
+    private ListPreference mHomeDoubleTapAction;
+    private ListPreference mMenuPressAction;
+    private ListPreference mMenuLongPressAction;
+    private ListPreference mMenuDoubleTapAction;
+    private ListPreference mBackPressAction;
+    private ListPreference mBackDoubleTapAction;
+
+    private PreferenceCategory mNavigationPreferencesCat;
+
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.button_settings);
+
+        final Resources res = getResources();
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        final int deviceKeys = getResources().getInteger(
+                com.android.internal.R.integer.config_deviceHardwareKeys);
+
+        final boolean hasPowerKey = KeyCharacterMap.deviceHasKey(KeyEvent.KEYCODE_POWER);
+        final boolean hasHomeKey = (deviceKeys & KEY_MASK_HOME) != 0;
+        final boolean hasMenuKey = (deviceKeys & KEY_MASK_MENU) != 0;
+        final boolean hasAssistKey = (deviceKeys & KEY_MASK_ASSIST) != 0;
+        final boolean hasBackKey = (deviceKeys & KEY_MASK_BACK) != 0;
+
+        boolean hasAnyBindableKey = false;
+        final PreferenceCategory homeCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_HOME);
+        final PreferenceCategory menuCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_MENU);
+        final PreferenceCategory backCategory =
+                (PreferenceCategory) prefScreen.findPreference(CATEGORY_BACK);
+
+        mHandler = new Handler();
+
+        if (hasHomeKey) {
+            int defaultLongPressAction = res.getInteger(
+                    com.android.internal.R.integer.config_longPressOnHomeBehavior);
+            if (defaultLongPressAction < ACTION_NOTHING ||
+                    defaultLongPressAction > ACTION_IN_APP_SEARCH) {
+                defaultLongPressAction = ACTION_NOTHING;
+            }
+
+            int defaultDoubleTapAction = res.getInteger(
+                    com.android.internal.R.integer.config_doubleTapOnHomeBehavior);
+            if (defaultDoubleTapAction < ACTION_NOTHING ||
+                    defaultDoubleTapAction > ACTION_IN_APP_SEARCH) {
+                defaultDoubleTapAction = ACTION_NOTHING;
+            }
+
+            int longPressAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_HOME_LONG_PRESS_ACTION,
+                    defaultLongPressAction);
+            mHomeLongPressAction = initActionList(KEY_HOME_LONG_PRESS, longPressAction);
+
+            int doubleTapAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_HOME_DOUBLE_TAP_ACTION,
+                    defaultDoubleTapAction);
+            mHomeDoubleTapAction = initActionList(KEY_HOME_DOUBLE_TAP, doubleTapAction);
+
+            hasAnyBindableKey = true;
+        } else {
+            prefScreen.removePreference(homeCategory);
+        }
+
+        if (hasMenuKey) {
+            int pressAction = Settings.System.getInt(resolver,
+                    Settings.System.KEY_MENU_ACTION, ACTION_MENU);
+            mMenuPressAction = initActionList(KEY_MENU_PRESS, pressAction);
+
+            int longPressAction = Settings.System.getInt(resolver,
+                        Settings.System.KEY_MENU_LONG_PRESS_ACTION,
+                        hasAssistKey ? ACTION_NOTHING : ACTION_SEARCH);
+            mMenuLongPressAction = initActionList(KEY_MENU_LONG_PRESS, longPressAction);
+
+            hasAnyBindableKey = true;
+        } else {
+            prefScreen.removePreference(menuCategory);
+        }
+
+        if (!hasBackKey) {
+            prefScreen.removePreference(backCategory);
+        }
+    }
+
+    private ListPreference initActionList(String key, int value) {
+        ListPreference list = (ListPreference) getPreferenceScreen().findPreference(key);
+        list.setValue(Integer.toString(value));
+        list.setSummary(list.getEntry());
+        list.setOnPreferenceChangeListener(this);
+        return list;
+    }
+
+    private void handleActionListChange(ListPreference pref, Object newValue, String setting) {
+        String value = (String) newValue;
+        int index = pref.findIndexOfValue(value);
+
+        pref.setSummary(pref.getEntries()[index]);
+        Settings.System.putInt(getContentResolver(), setting, Integer.valueOf(value));
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.DUMPSTER;
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mHomeLongPressAction) {
+            handleActionListChange(mHomeLongPressAction, newValue,
+                    Settings.System.KEY_HOME_LONG_PRESS_ACTION);
+            return true;
+        } else if (preference == mHomeDoubleTapAction) {
+            handleActionListChange(mHomeDoubleTapAction, newValue,
+                    Settings.System.KEY_HOME_DOUBLE_TAP_ACTION);
+            return true;
+        } else if (preference == mMenuPressAction) {
+            handleActionListChange(mMenuPressAction, newValue,
+                    Settings.System.KEY_MENU_ACTION);
+            return true;
+        } else if (preference == mMenuLongPressAction) {
+            handleActionListChange(mMenuLongPressAction, newValue,
+                    Settings.System.KEY_MENU_LONG_PRESS_ACTION);
+            return true;
+        }
+        return false;
+    }
+
+}