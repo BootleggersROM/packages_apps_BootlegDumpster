a97355088ae2dd08382dba576688683602269ca9
ElDainosor <eldainosor@gmail.com>
2018-01-11 01:17:21 +0000

    Add our Bootleggers About.
    This will be an about section for our ROM with extras and stuff. Credits where it's due:
    
    https://github.com/ValidusOs/packages_apps_Settings/commit/b33489de43da6198ab3bd97f0c160985fc1b1a19
    https://github.com/AospExtended/platform_packages_apps_Settings/commit/836fe44b437ddbcd1881a8bdfcc504fed43a122a
    https://github.com/AospExtended/platform_packages_apps_Settings/commit/7d0ca1d8df127fe17e4ec893d9605734374e1b97
    https://github.com/ResurrectionRemix/Resurrection_packages_apps_Settings/commit/58b66cff6c84c5e7a55e266c371bc7ff05a7946d
    
    This include the section of About Bootleggers and those respective fixes

---
+++ src/com/bootleggers/dumpster/external/AboutMore.java
@@ -0,0 +1,51 @@
+package com.bootleggers.dumpster.external;
+
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.os.Vibrator;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+import android.text.TextUtils;
+
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+public class AboutMore extends SettingsPreferenceFragment {
+
+    private Preference prefCrowd;
+    private String bootlegCrowdinString;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.bootleg_external_aboutmore);
+        Resources res = getResources();
+        prefCrowd = (Preference) findPreference("bootleg_morecd");
+
+        bootlegCrowdinString = res.getString(R.string.bootleg_moarstuff_cd_sumgen, res.getString(R.string.bootleg_moarstuff_cd_summary01), res.getString(R.string.bootleg_moarstuff_cd_summary02));
+
+        prefCrowd.setSummary(String.valueOf(bootlegCrowdinString));
+
+        mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.bootleg_pref_moarstuff_wewsecret);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.BOOTLEG;
+    }
+
+}
