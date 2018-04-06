3e0d7a0ca82093ab39f883fdba14945631135d1d
El Dainosor <eldainosor@gmail.com>
2018-04-06 06:47:54 +0000

    [1/2] base: Set GoBold font as default LS font

--- src/com/bootleggers/dumpster/fragments/LockScreenSettings.java
+++ src/com/bootleggers/dumpster/fragments/LockScreenSettings.java
@@ -70,7 +70,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
 
         mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
         mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
-                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 0)));
+                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 12)));
         mLockClockFonts.setSummary(mLockClockFonts.getEntry());
         mLockClockFonts.setOnPreferenceChangeListener(this);
 
