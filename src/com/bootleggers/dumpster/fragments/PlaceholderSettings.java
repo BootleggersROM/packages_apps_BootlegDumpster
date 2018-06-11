73fdc5ca95b24ad36df18ae4e80062d9ae8afd1b
ElDainosor <eldainosor@gmail.com>
2018-06-11 12:13:53 +0000

    dumpster: add a placeholder files
    just to keep it handy in case we need some more

---
+++ src/com/bootleggers/dumpster/fragments/PlaceholderSettings.java
@@ -0,0 +1,24 @@
+package com.bootleggers.dumpster.fragments;
+
+import com.android.internal.logging.nano.MetricsProto;
+
+import android.os.Bundle;
+import com.android.settings.R;
+
+import com.android.settings.SettingsPreferenceFragment;
+
+public class PlaceholderSettings extends SettingsPreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.bootleg_dumpster_gestures);
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsProto.MetricsEvent.BOOTLEG;
+    }
+
+}
