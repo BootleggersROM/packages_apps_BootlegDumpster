06eb0863a28dad76c69232a50c2cc6db7936890f
Varun Date <date.varun123@gmail.com>
2018-08-03 21:36:05 +0000

    Keyguard & Bouncer Alpha [2/2]
    Forward Port from RR Nougat.
    
    Android Oreo bringup made by @SuperDroidBond

--- src/com/bootleggers/dumpster/external/LockscreenUI.java
+++ src/com/bootleggers/dumpster/external/LockscreenUI.java
@@ -34,8 +34,12 @@ public class LockscreenUI extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
     private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+    private static final String LOCKSCREEN_SECURITY_ALPHA = "lockscreen_security_alpha";
+    private static final String LOCKSCREEN_ALPHA = "lockscreen_alpha";
 
     ListPreference mLockClockFonts;
+    private CustomSeekBarPreference mLsAlpha;
+    private CustomSeekBarPreference mLsSecurityAlpha;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,6 +55,18 @@ public class LockscreenUI extends SettingsPreferenceFragment implements
                 getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 0)));
         mLockClockFonts.setSummary(mLockClockFonts.getEntry());
         mLockClockFonts.setOnPreferenceChangeListener(this);
+
+        mLsSecurityAlpha = (CustomSeekBarPreference) findPreference(LOCKSCREEN_SECURITY_ALPHA);
+        float alpha2 = Settings.System.getFloat(resolver,
+                Settings.System.LOCKSCREEN_SECURITY_ALPHA, 0.75f);
+        mLsSecurityAlpha.setValue((int)(100 * alpha2));
+        mLsSecurityAlpha.setOnPreferenceChangeListener(this);
+
+        mLsAlpha = (CustomSeekBarPreference) findPreference(LOCKSCREEN_ALPHA);
+        float alpha = Settings.System.getFloat(resolver,
+                Settings.System.LOCKSCREEN_ALPHA, 0.45f);
+        mLsAlpha.setValue((int)(100 * alpha));
+        mLsAlpha.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -60,6 +76,16 @@ public class LockscreenUI extends SettingsPreferenceFragment implements
                     Integer.valueOf((String) newValue));
             mLockClockFonts.setValue(String.valueOf(newValue));
             mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+            return true;
+        } else if (preference == mLsSecurityAlpha) {
+            int alpha2 = (Integer) newValue;
+            Settings.System.putFloat(resolver,
+                    Settings.System.LOCKSCREEN_SECURITY_ALPHA, alpha2 / 100.0f);
+            return true;
+        } else if (preference == mLsAlpha) {
+            int alpha = (Integer) newValue;
+            Settings.System.putFloat(resolver,
+                    Settings.System.LOCKSCREEN_ALPHA, alpha / 100.0f);
             return true;
         }
         return false;
