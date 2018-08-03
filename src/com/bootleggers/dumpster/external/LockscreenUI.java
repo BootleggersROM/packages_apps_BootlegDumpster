652800263dd1ebc0bb486a44aa6abbaa37f2fc09
Alex Cruz <mazdarider23@gmail.com>
2018-08-03 20:14:40 +0000

    Lockscreen Clock Widget Fonts [2/2]
    Change-Id: I84165a6207fafa6f1e2c9368ea58392c86f3abb3
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/bootleggers/dumpster/external/LockscreenUI.java
+++ src/com/bootleggers/dumpster/external/LockscreenUI.java
@@ -33,6 +33,10 @@ import com.android.internal.logging.nano.MetricsProto;
 public class LockscreenUI extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
+
+    ListPreference mLockClockFonts;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,10 +45,23 @@ public class LockscreenUI extends SettingsPreferenceFragment implements
         ContentResolver resolver = getActivity().getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         Resources resources = getResources();
+
+        mLockClockFonts = (ListPreference) findPreference(LOCK_CLOCK_FONTS);
+        mLockClockFonts.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.LOCK_CLOCK_FONTS, 0)));
+        mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+        mLockClockFonts.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mLockClockFonts) {
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_CLOCK_FONTS,
+                    Integer.valueOf((String) newValue));
+            mLockClockFonts.setValue(String.valueOf(newValue));
+            mLockClockFonts.setSummary(mLockClockFonts.getEntry());
+            return true;
+        }
         return false;
     }
 
