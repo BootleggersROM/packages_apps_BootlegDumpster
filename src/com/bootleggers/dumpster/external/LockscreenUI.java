f9737bb40a928fa11909b899277d5fc67ca072fc
ElDainosor <eldainosor@gmail.com>
2018-08-03 22:27:32 +0000

    [2/2] dumpster: Lockscreen Owner info fonts
    Once again, thanks DU, PN and RR for the main code

--- src/com/bootleggers/dumpster/external/LockscreenUI.java
+++ src/com/bootleggers/dumpster/external/LockscreenUI.java
@@ -35,9 +35,11 @@ public class LockscreenUI extends SettingsPreferenceFragment implements
 
     private static final String LOCK_CLOCK_FONTS = "lock_clock_fonts";
     private static final String LOCK_DATE_FONTS = "lock_date_fonts";
+    private static final String LOCK_OWNER_FONTS = "lock_owner_fonts";
 
     ListPreference mLockClockFonts;
     ListPreference mLockDateFonts;
+    ListPreference mLockOwnerFonts;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -60,6 +62,11 @@ public class LockscreenUI extends SettingsPreferenceFragment implements
         mLockDateFonts.setSummary(mLockDateFonts.getEntry());
         mLockDateFonts.setOnPreferenceChangeListener(this);
 
+        mLockOwnerFonts = (ListPreference) findPreference(LOCK_OWNER_FONTS);
+        mLockOwnerFonts.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.LOCK_OWNER_FONTS, 8)));
+        mLockOwnerFonts.setSummary(mLockOwnerFonts.getEntry());
+        mLockOwnerFonts.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
@@ -75,6 +82,12 @@ public class LockscreenUI extends SettingsPreferenceFragment implements
                     Integer.valueOf((String) newValue));
             mLockDateFonts.setValue(String.valueOf(newValue));
             mLockDateFonts.setSummary(mLockDateFonts.getEntry());
+            return true;
+        } else if (preference == mLockOwnerFonts) {
+            Settings.System.putInt(getContentResolver(), Settings.System.LOCK_OWNER_FONTS,
+                    Integer.valueOf((String) newValue));
+            mLockOwnerFonts.setValue(String.valueOf(newValue));
+            mLockOwnerFonts.setSummary(mLockOwnerFonts.getEntry());
             return true;
         }
         return false;
