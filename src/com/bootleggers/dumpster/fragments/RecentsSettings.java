20cf6766a8d0a522701b7e6216685674a823ee97
Shubham Singh <coolsks94@gmail.com>
2018-10-10 00:09:21 +0000

    Dumpster: Allow users to switch between Pie/Oreo recents [2/3]
    Change-Id: Ic65c2e5d2c7dae8527e2261af6942e3ae1c0d959
    Signed-off-by: Shubham Singh <coolsks94@gmail.com>

--- src/com/bootleggers/dumpster/fragments/RecentsSettings.java
+++ src/com/bootleggers/dumpster/fragments/RecentsSettings.java
@@ -20,9 +20,13 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 import com.android.settings.SettingsPreferenceFragment;
+import com.bootleggers.dumpster.extra.Utils;
 
 public class RecentsSettings extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
+
+    private ListPreference mRecentsComponentType;
+    private static final String RECENTS_COMPONENT_TYPE = "recents_component";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -32,13 +36,33 @@ public class RecentsSettings extends SettingsPreferenceFragment implements
 
         ContentResolver resolver = getActivity().getContentResolver();
 
+        // recents component type
+        mRecentsComponentType = (ListPreference) findPreference(RECENTS_COMPONENT_TYPE);
+        int type = Settings.System.getInt(resolver,
+                Settings.System.RECENTS_COMPONENT, 0);
+        mRecentsComponentType.setValue(String.valueOf(type));
+        mRecentsComponentType.setSummary(mRecentsComponentType.getEntry());
+        mRecentsComponentType.setOnPreferenceChangeListener(this);
+
+
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
-
+        if (preference == mRecentsComponentType) {
+            int type = Integer.valueOf((String) objValue);
+            int index = mRecentsComponentType.findIndexOfValue((String) objValue);
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.RECENTS_COMPONENT, type);
+            mRecentsComponentType.setSummary(mRecentsComponentType.getEntries()[index]);
+            if (type == 1) { // Disable swipe up gesture, if oreo type selected
+               Settings.Secure.putInt(getActivity().getContentResolver(),
+                    Settings.Secure.SWIPE_UP_TO_SWITCH_APPS_ENABLED, 0);
+            }
+            Utils.restartSystemUi(getContext());
+        return true;
+        }
     return false;
-
     }
 
     @Override
