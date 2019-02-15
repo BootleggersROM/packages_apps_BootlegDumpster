c85db0c975b20f397686683aa7bd619f3943e210
Alex <du.alexcruz@gmail.com>
2019-02-15 16:33:59 -0500

    Active edge settings [3/6]
    Change-Id: I8fb0a4d78d4cc442e5f5f9e2f755d7db49a13303

--- src/com/bootleggers/dumpster/fragments/GestureSettings.java
+++ src/com/bootleggers/dumpster/fragments/GestureSettings.java
@@ -3,17 +3,31 @@ package com.bootleggers.dumpster.fragments;
 import com.android.internal.logging.nano.MetricsProto;
 
 import android.os.Bundle;
+import android.support.v7.preference.Preference;
 import com.android.settings.R;
 
 import com.android.settings.SettingsPreferenceFragment;
 
 public class GestureSettings extends SettingsPreferenceFragment {
 
+    private static final String ACTIVE_EDGE_CATEGORY = "active_edge_category";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.bootleg_dumpster_gestures);
+
+        Preference ActiveEdge = findPreference(ACTIVE_EDGE_CATEGORY);
+        if (!getResources().getBoolean(R.bool.has_active_edge)) {
+            getPreferenceScreen().removePreference(ActiveEdge);
+        } else {
+            if (!getContext().getPackageManager().hasSystemFeature(
+                    "android.hardware.sensor.assist")) {
+                getPreferenceScreen().removePreference(ActiveEdge);
+            }
+        }
+
     }
 
     @Override
