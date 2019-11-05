799d2998edbdc904820880930c6711a27d7bb75a
El Dainosor <eldainosor@gmail.com>
2019-11-05 23:46:26 +0000

    Dumpster: toggle privacy chip on QS
    This is a new addition from AOSP, that displays on your statusbar when some app uses some privacy-concerning permission, like microphone, camera or location.
    Just make it toggleable for the big notch people.

--- src/com/bootleggers/dumpster/fragments/QuickSettings.java
+++ src/com/bootleggers/dumpster/fragments/QuickSettings.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.UserHandle;
+import android.provider.DeviceConfig;
 import android.content.ContentResolver;
 import android.content.res.Resources;
 import androidx.preference.ListPreference;
@@ -21,12 +22,17 @@ import java.util.Locale;
 import android.text.TextUtils;
 import android.view.View;
 
+import com.android.internal.config.sysui.SystemUiDeviceConfigFlags;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class QuickSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private SwitchPreference mQSPrivPill;
+
+    private static final String QS_PRIVACY_PILL = "qs_show_privacy_chip";
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -36,12 +42,23 @@ public class QuickSettings extends SettingsPreferenceFragment implements
 
         PreferenceScreen prefScreen = getPreferenceScreen();
         ContentResolver resolver = getActivity().getContentResolver();
+
+        mQSPrivPill = (SwitchPreference) findPreference(QS_PRIVACY_PILL);
+        boolean pillBool = DeviceConfig.getBoolean(DeviceConfig.NAMESPACE_PRIVACY,
+                SystemUiDeviceConfigFlags.PROPERTY_PERMISSIONS_HUB_ENABLED, false);
+        mQSPrivPill.setOnPreferenceChangeListener(this);
+        mQSPrivPill.setChecked(pillBool);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         switch (preference.getKey()) {
+            case QS_PRIVACY_PILL:
+                boolean isEnabled = (Boolean) newValue;
+                DeviceConfig.setProperty(DeviceConfig.NAMESPACE_PRIVACY,
+                SystemUiDeviceConfigFlags.PROPERTY_PERMISSIONS_HUB_ENABLED, isEnabled ? "true" : "false", false);
+                return true;
             default:
                 return false;
         }
