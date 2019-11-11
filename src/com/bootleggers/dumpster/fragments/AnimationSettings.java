5c4fc386a9c244bed7dbff29c8f0e8a7afa51b49
maxwen <max.weninger@gmail.com>
2019-11-11 05:43:36 +0000

    Dumpster: screen off animation config [2/2]
    Change-Id: Iafeed01b34e7a2e1db98d92f44a6579da2166535

--- src/com/bootleggers/dumpster/fragments/AnimationSettings.java
+++ src/com/bootleggers/dumpster/fragments/AnimationSettings.java
@@ -25,17 +25,36 @@ import com.android.internal.logging.nano.MetricsProto;
 public class AnimationSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
+    private static final String KEY_SCREEN_OFF_ANIMATION = "screen_off_animation";
+
+    private Context mContext;
+    private ListPreference mScreenOffAnimation;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.bootleg_dumpster_animations);
         mFooterPreferenceMixin.createFooterPreference().setTitle(R.string.animations_transparent_alert);
+
+        mScreenOffAnimation = (ListPreference) findPreference(KEY_SCREEN_OFF_ANIMATION);
+        int screenOffAnimation = Settings.System.getInt(getContentResolver(),
+                Settings.System.SCREEN_OFF_ANIMATION, 0);
+        mScreenOffAnimation.setValue(Integer.toString(screenOffAnimation));
+        mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntry());
+        mScreenOffAnimation.setOnPreferenceChangeListener(this);
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         ContentResolver resolver = getActivity().getContentResolver();
         switch (preference.getKey()) {
+
+            case KEY_SCREEN_OFF_ANIMATION:
+                int value = Integer.valueOf((String) newValue);
+                int index = mScreenOffAnimation.findIndexOfValue((String) newValue);
+                mScreenOffAnimation.setSummary(mScreenOffAnimation.getEntries()[index]);
+                Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_OFF_ANIMATION, value);
+                return true;
             default:
                 return false;
         }
