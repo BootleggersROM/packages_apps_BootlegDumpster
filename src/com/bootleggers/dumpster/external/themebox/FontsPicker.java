aab9d97284562e03c6a004230e278b5b7087df42
Pranav Vashi <neobuddy89@gmail.com>
2022-02-19 22:46:09 +0530

    dumpster: Update for ThemeUtils changes
    * And some clean up!
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I1dfe99cf3236d6717db612da8a0689b389492ed6

--- src/com/bootleggers/dumpster/external/themebox/FontsPicker.java
+++ src/com/bootleggers/dumpster/external/themebox/FontsPicker.java
@@ -37,7 +37,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.text.TextUtils;
 import androidx.preference.PreferenceViewHolder;
 import android.view.ViewGroup.LayoutParams;
@@ -47,8 +46,6 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import androidx.recyclerview.widget.RecyclerView;
-import android.net.Uri;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceScreen;
@@ -62,7 +59,6 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.util.bootleg.ThemeUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Arrays;
 
@@ -84,7 +80,6 @@ public class FontsPicker extends SettingsPreferenceFragment {
 
         mThemeUtils = new ThemeUtils(getActivity());
         mPkgs = mThemeUtils.getOverlayPackagesForCategory(mCategory, "android");
-        Collections.sort(mPkgs);
     }
 
     @Override
@@ -217,6 +212,6 @@ public class FontsPicker extends SettingsPreferenceFragment {
     }
 
     public void enableOverlays(int position) {
-        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position));
+        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position), "android");
     }
 }
--- src/com/bootleggers/dumpster/external/themebox/StatusBarIcons.java
+++ src/com/bootleggers/dumpster/external/themebox/StatusBarIcons.java
@@ -33,7 +33,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.FrameLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.text.TextUtils;
 import androidx.preference.PreferenceViewHolder;
 import android.view.ViewGroup.LayoutParams;
@@ -43,8 +42,6 @@ import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import androidx.recyclerview.widget.RecyclerView;
-import android.net.Uri;
-import androidx.core.content.res.ResourcesCompat;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
 import androidx.preference.PreferenceScreen;
@@ -58,7 +55,6 @@ import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.util.bootleg.ThemeUtils;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -90,7 +86,6 @@ public class StatusBarIcons extends SettingsPreferenceFragment {
 
         mThemeUtils = new ThemeUtils(getActivity());
         mPkgs = mThemeUtils.getOverlayPackagesForCategory(mCategory, "android");
-        Collections.sort(mPkgs);
     }
 
     @Override
@@ -209,10 +204,6 @@ public class StatusBarIcons extends SettingsPreferenceFragment {
             Resources res = pkg.equals("android") ? Resources.getSystem()
                     : pm.getResourcesForApplication(pkg);
             int resId = res.getIdentifier(drawableName, "drawable", pkg);
-            if (resId == 0) {
-                return Resources.getSystem().getDrawable(
-                        Resources.getSystem().getIdentifier(drawableName, "drawable", "android"));
-            }
             return res.getDrawable(resId);
         }
         catch (PackageManager.NameNotFoundException e) {
@@ -233,7 +224,7 @@ public class StatusBarIcons extends SettingsPreferenceFragment {
     }
 
     public void enableOverlays(int position) {
-        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position));
+        mThemeUtils.setOverlayEnabled(mCategory, mPkgs.get(position), "android");
         String pattern = "android".equals(mPkgs.get(position)) ? ""
                 : mPkgs.get(position).split("\\.")[4];
         for (Map.Entry<String, String> entry : overlayMap.entrySet()) {
@@ -243,12 +234,12 @@ public class StatusBarIcons extends SettingsPreferenceFragment {
 
     public void enableOverlay(String category, String target, String pattern) {
         if (pattern.isEmpty()) {
-            mThemeUtils.setOverlayEnabled(category, "android");
+            mThemeUtils.setOverlayEnabled(category, "android", "android");
             return;
         }
         for (String pkg: mThemeUtils.getOverlayPackagesForCategory(category, target)) {
             if (pkg.contains(pattern)) {
-                mThemeUtils.setOverlayEnabled(category, pkg);
+                mThemeUtils.setOverlayEnabled(category, pkg, target);
             }
         }
     }
