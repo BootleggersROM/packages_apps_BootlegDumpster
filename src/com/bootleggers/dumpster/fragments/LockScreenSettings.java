f950940d568b053b16af6e04169a40050ce72d8a
Ido Ben-Hur <idoybh2@gmail.com>
2023-01-16 14:07:07 +0200

    dumpster: LockScreenSettings: Avoid joining strings with + in a loop
    Properly use the StringBuilder there

--- src/com/bootleggers/dumpster/fragments/LockScreenSettings.java
+++ src/com/bootleggers/dumpster/fragments/LockScreenSettings.java
@@ -106,11 +106,11 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
         if (value == null || value.isEmpty()) {
             StringBuilder sb = new StringBuilder(DEFAULT_START_SHORTCUT[0]);
             for (int i = 1; i < DEFAULT_START_SHORTCUT.length; i++) {
-                sb.append("," + DEFAULT_START_SHORTCUT[i]);
+                sb.append(",").append(DEFAULT_START_SHORTCUT[i]);
             }
             sb.append(";" + DEFAULT_END_SHORTCUT[0]);
             for (int i = 1; i < DEFAULT_END_SHORTCUT.length; i++) {
-                sb.append("," + DEFAULT_END_SHORTCUT[i]);
+                sb.append(",").append(DEFAULT_END_SHORTCUT[i]);
             }
             value = sb.toString();
         }
@@ -137,7 +137,7 @@ public class LockScreenSettings extends SettingsPreferenceFragment implements
             final String[] def = start ? DEFAULT_START_SHORTCUT : DEFAULT_END_SHORTCUT;
             for (String str : def) {
                 if (str.equals(value)) continue;
-                sb.append("," + str);
+                sb.append(",").append(str);
             }
             split[splitIndex] = sb.toString();
         }
